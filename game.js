35a4c6b2c413fe8ca0ad5ecfb3e323e0ad872eb3
cassidybeni <cassidybeni@pursuit.org>
2020-12-17 14:37:35 -0500

    Complete game.js

--- game.js
+++ game.js
@@ -7,7 +7,14 @@ const rls = require('readline-sync')
  * @returns {undefined}
  */
 const startGame = () => {
-
+  console.log("Welcome!")
+  if(rls.keyInYN("Would you like to play?")){
+    console.log("Let's start!")
+    gameLoop()
+  } else {
+    console.log("Have a nice life!")
+    quitGame()
+  }
 }
 
 /**
@@ -17,7 +24,8 @@ const startGame = () => {
  * @returns {undefined}
  */
 const quitGame = () => {
-
+  console.log("Goodbye!")
+  process.exit()
 }
 
 /**
@@ -28,7 +36,9 @@ const quitGame = () => {
  * @returns {undefined}
  */
 const gameLoop = () => {
-
+  console.log("I have a random number in mind")
+  console.log("It's between 1 and 1000")
+  console.log("You have 10 guesses total")
 }
 
 
@@ -38,7 +48,10 @@ const gameLoop = () => {
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
-
+  for(let i = 0; i <= 1000; i++){
+    randomNum = Math.random() * i
+  }
+  return randomNum
 }
 
 startGame()
