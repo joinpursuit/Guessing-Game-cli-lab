3578ca3f657c63ca8fa7312d10d1eb2b83ebb40c
Kathy Gee <mzchick@gmail.com>
2020-12-18 18:18:31 -0500

    all tests passed but game still wonky

--- game.js
+++ game.js
@@ -7,9 +7,6 @@ const rls = require('readline-sync')
  * @returns {undefined}
  */
 const startGame = () => {
-  //prompt user if they want to play
-  //if yes, call gameLoop()
-  //else, call quitGame()
   console.log("Welcome!")
   
   let answer = rls.keyInYN("Ready?")
@@ -42,13 +39,57 @@ const quitGame = () => {
  * 
  * @returns {undefined}
  */
-const gameLoop = () => {
 
-}
+// * Generate a random number and store it in a variable
+// * Take an input (guess) from the user using `rls.questionInt()`
+// * If the guess is correct, log "Congrats! You got it right!"
+//   * Prompt the user if they want to play again
+//     * if Y, call `gameLoop()`
+//     * if N, call `quitGame()`
+// * If the guess is high, log "Your guess is too high"
+// * If the guess is low, log "Your guess is too low"
+// * Starting with 10, decrease the number of guesses after each attempt
+// * If the number of guesses reaches 0, log "You lose!" and call `quitGame()
+ // startGame()
+  //   let guessCount = 10
 
+  // while (guessCount>10){
+  
+  // else{
+  //   console.log("Your guess is too high")
+  //   console.log("Your guess is too low")
+  // }
+  // guessCount -= 1
+  // }
 
+  const gameLoop = () => {
+    generateRandomNumber()
+    console.log("I have a random number in mind")
+    console.log("It's between 1 and 1000")
+    console.log("You have 10 guesses total")
+  }
+  
+  const guess = rls.questionInt()
+  
+  // if (guess === generateRandomNumber){
+  //   console.log("Congrats! You got it right!")
+  //   answer(rls.keyInYN="Would you like to play again?")
+  //   if(answer) {
+  //     console.log("Let's start!")
+  //     gameLoop()
+  //   }
+  //     else{
+  //       quitGame()
+  //     }
+  // }
+  // else if (guess > generateRandomNumber){
+  //   console.log("Your guess is too high")
+  // }
+  //   else if( guess < generateRandomNumber){
+  //   console.log("Your guess is too low")
+  // }
 /***
- * Generates a random number 
+ * Generates a random number
  *
  * @returns {number} - a number between 1 and 1000
  */
