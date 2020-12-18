b6aeac2855c693790a0e605adac452c153a98be5
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2020-12-17 23:32:35 -0500

    first attempt at games.js

--- game.js
+++ game.js
@@ -7,7 +7,15 @@ const rls = require('readline-sync')
  * @returns {undefined}
  */
 const startGame = () => {
-
+  let answer = rls.keyInYN("Ready?")
+  if(answer) {
+    console.log("Let's start!")
+    gameLoop()
+  }
+  else {
+    console.log('Have a nice life!')
+    quitGame()
+  }
 }
 
 /**
@@ -17,6 +25,8 @@ const startGame = () => {
  * @returns {undefined}
  */
 const quitGame = () => {
+  console.log("Goodbye!")
+  process.exit()
 
 }
 
@@ -28,6 +38,36 @@ const quitGame = () => {
  * @returns {undefined}
  */
 const gameLoop = () => {
+  console.log("I have a random number in mind\n")
+  console.log("It's between 1 and 1000\n")
+  console.log("You have 10 guesses\n")
+let randNum = generateRandomNumber()
+let guessCount = 10
+let guess = rls.questionInt() 
+while(guessCount < 10) {
+if(guess = randNum) {
+  console.log("Congrats! You got it right!") 
+    if(rls.keyInYN("Want to play again?\n")) {
+       gameLoop()
+    }
+    else {
+        quitGame()
+    }
+}
+else if(guess > randNum) {
+  console.log("Your guess is too high")
+  guessCount --
+}
+else if (guess < randNum) {
+  console.log("Your guess is too low")
+  guessCount --
+}
+
+}
+
+
+
+
 
 }
 
@@ -38,6 +78,7 @@ const gameLoop = () => {
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
+  return (Math.random()* 1000)
 
 }
 
