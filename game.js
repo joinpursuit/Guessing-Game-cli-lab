132d54a437121b26e4f9ee0d0b064d77b073da06
Grace Bong <gracebong.park@gmail.com>
2020-12-17 23:52:38 -0500

    partially completed lab

--- game.js
+++ game.js
@@ -7,7 +7,17 @@ const rls = require('readline-sync')
  * @returns {undefined}
  */
 const startGame = () => {
+  console.log('Welcome!!')
+  let answer = rls.keyInYN('Wanna play a game?')
 
+  if (answer) {  
+    console.log('Let\'s start!')
+    gameLoop()
+  }
+  else {
+    console.log('Have a nice life!')
+    quitGame()
+  }    
 }
 
 /**
@@ -17,9 +27,9 @@ const startGame = () => {
  * @returns {undefined}
  */
 const quitGame = () => {
-
+  console.log('Goodbye!')
+  process.exit()
 }
-
 /**
  * Controls the flow of the game.
  * Should prompt the user to play again once all
@@ -28,9 +38,57 @@ const quitGame = () => {
  * @returns {undefined}
  */
 const gameLoop = () => {
-
+  console.log('I have a random number in mind')
+  console.log('It\'s between 1 and 1000')
+  console.log('You have 10 guesses')
+  const guessLoop = []
+  for (let i = 10; i > 0; i -= 1) {
+      guessLoop.unshift(i)
+      console.log(`You have ${guessLoop[0]} attempts left!`)
+      gameLoop()
+  }
+    if (i === 0) {
+      console.log('You lose!')
+      quitGame()
+    }
+  return i -= 1
 }
 
+const randomNumber = generateRandomNumber ()
+let userInput = rls.questionInt('Choose a number between 1 and 1000 \n')
+  
+    if (userInput === randomNumber) {
+      console.log('Congrats! You got it right!')
+      let anotherRound = rls.keyInYN('Wanna play another round? \n')
+        if (anotherRound) {
+          gameLoop()
+        }
+        else {
+          quitGame()
+        }
+    }
+    else if (userInput > randomNumber){
+      console.log('Your guess is too high')
+      gameLoop()
+    }
+    else {
+      console.log('Your guess is too low')
+      gameLoop()
+    }
+      
+    // const attempt = (i) => {
+    //     while (i > 0) {
+    //       i -= 1
+    //       console.log(`You have ${i} attempts left!`)
+    //       gameLoop()
+    //     }
+    //     if (i === 0) {
+    //       console.log('You lose!')
+    //       quitGame()
+    //     }
+    //   }
+
+//}
 
 /***
  * Generates a random number 
@@ -38,8 +96,11 @@ const gameLoop = () => {
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
-
+  return Math.round(Math.random() * (1-1000) + 1)
 }
+// const generateRandomNumber = () => {
+//   return Math.round(Math.random() * (1-1000) + 1)
+// }
 
 startGame()
 
