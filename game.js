958beae8b187be15150d14dabbe6ca53aa75d33a
Candice Agard <Candice.Agard@gmail.com>
2020-12-17 21:22:47 -0500

    Commit of test completion

--- game.js
+++ game.js
@@ -1,51 +1,73 @@
-const rls = require('readline-sync')
+const rls = require("readline-sync");
 
 /**
  * Starts the game by prompting the user if they want to play
  * Calls either gameLoop() or quitGame()
- * 
+ *
  * @returns {undefined}
  */
 const startGame = () => {
+  console.log("Welcome!");
 
-}
+  let answer = rls.keyInYN("Ready?");
+  if (answer) {
+    console.log("Let's start!");
+    gameLoop();
+  } else {
+    console.log("Have a nice life!");
+    quitGame();
+  }
+};
 
 /**
  * Logs "Goodbye!"
  * Calls process.exit() to quit the game
- * 
+ *
  * @returns {undefined}
  */
 const quitGame = () => {
-
-}
+  console.log("Goodbye!");
+  process.exit();
+};
 
 /**
  * Controls the flow of the game.
  * Should prompt the user to play again once all
  * guesses have been made or correct answer guessed
- * 
+ *
  * @returns {undefined}
  */
 const gameLoop = () => {
+  console.log("I have a random number in mind");
+  console.log("It's between 1 and 1000");
+  console.log("You have 10 guesses total");
+  let generateRandomNumber = Math.random(1 * 1000);
+  let answer = rls.questionInt("Pick a number! \n");
 
-}
-
+  if ((answer = generateRandomNumber)) {
+    console.log("Congrats! You got it right!");
+  } else if (answer >= generateRandomNumber) {
+    console.log("Your guess is too low");
+  } else {
+    console.log("You lose!");
+  }
+  gameLoop()
+};
 
 /***
- * Generates a random number 
+ * Generates a random number
  *
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
+  return Math.round(Math.random());
+};
 
-}
-
-startGame()
+startGame();
 
 module.exports = {
   startGame,
   quitGame,
   gameLoop,
-  generateRandomNumber
-}
+  generateRandomNumber,
+};
