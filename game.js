9574ed408916005c451ec04cdbaa9152b59c5387
joshuamendez <joshuafmendez@gmail.com>
2020-12-20 17:25:06 -0500

    completed bonus

--- game.js
+++ game.js
@@ -1,15 +1,20 @@
 const rls = require('readline-sync')
 
+let myArgs = process.argv.slice(2);
+guessCount = myArgs[0];
+min = myArgs[1];
+max = myArgs[2];
+
 /**
  * Starts the game by prompting the user if they want to play
  * Calls either gameLoop() or quitGame()
  * 
  * @returns {undefined}
  */
-const startGame = () => {
+const startGame = (guessCount, min, max) => {
   if(rls.keyInYN('Want to play a game?')){
     console.log('Let\'s start!');
-    gameLoop();
+    gameLoop(guessCount, min, max);
   }else{
     console.log('Have a nice life!');
     quitGame();
@@ -34,16 +39,13 @@ const quitGame = () => {
  * 
  * @returns {undefined}
  */
-const gameLoop = () => {
+const gameLoop = (guessCount = 10, min = 1, max = 1000) => {
   console.log('I have a random number in mind');
-  console.log('It\'s between 1 and 1000');
-  console.log("You have 10 guesses total");
-  let top = 1000;
-  let bottom = 1;
+  console.log(`It\'s between ${min} and ${max}`);
+  console.log(`You have ${guessCount} guesses total`);
   // Generate a random number and store it in a variable
-  // let randomNum = 5;
-  let randomNum = generateRandomNumber();
-  let guessCount = 10;
+  // let randomNum = 5; // this is def a random number
+  let randomNum = generateRandomNumber(min, max);
   // Take an input (guess) from the user using rls.questionInt()
   while(guessCount > 0){
     let guess = rls.questionInt();
@@ -58,10 +60,10 @@ const gameLoop = () => {
       }
       if(guess > randomNum){
         console.log('You\'re guess is too high.');
-        top = randomNum - 1;
+        max = randomNum - 1;
       }else{
         console.log('You\'re guess is too low.');
-        bottom = randomNum + 1;
+        min = randomNum + 1;
       }
     }
   }
@@ -89,13 +91,13 @@ const gameOver = humanWins => {
  *
  * @returns {number} - a number between 1 and 1000
  */
-const generateRandomNumber = () => {
-  let num = Math.floor(Math.random() * 1000) + 1;
-  console.log(num); // tested to make sure random
+const generateRandomNumber = (min, max) => {
+  let num = Math.floor(Math.random() * (max - min + 1) + min);
+  // console.log(num); // tested to make sure random *totally random number*
   return num;
 }
 
-startGame()
+startGame(guessCount, min, max);
 
 module.exports = {
   startGame,
