147fd2e9a4276414558b23143988bca2a3d22637
Marsha Hall <mphall77@gmail.com>
2020-12-20 17:49:32 -0500

    add games.js

--- game.js
+++ game.js
@@ -1,51 +1,60 @@
-const rls = require('readline-sync')
+const rls = require("readline-sync");
 
 /**
  * Starts the game by prompting the user if they want to play
  * Calls either gameLoop() or quitGame()
- * 
+ *
  * @returns {undefined}
  */
 const startGame = () => {
-
-}
+  if (rls.keyInYN("Do you want to play a game with me?")) {
+    console.log("Let's start!");
+    gameLoop();
+  } else {
+    console.log("Have a nice life!");
+    quitGame();
+  }
+};
 
 /**
  * Logs "Goodbye!"
  * Calls process.exit() to quit the game
- * 
+ *
  * @returns {undefined}
  */
 const quitGame = () => {
-
-}
+  console.log("Goodbye!");
+  process.exit();
+};
 
 /**
  * Controls the flow of the game.
  * Should prompt the user to play again once all
  * guesses have been made or correct answer guessed
- * 
+ *
  * @returns {undefined}
  */
 const gameLoop = () => {
-
-}
-
+  console.log("I have a random number in mind")
+  console.log("It's between 1 and 1000")
+  console.log("You have 10 guesses total")
+};
 
 /***
- * Generates a random number 
+ * Generates a random number
  *
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
+  let guess = Math.floor(Math.random() * 1000) + 1;
+  return guess;
+};
 
-}
-
-startGame()
+startGame();
 
 module.exports = {
   startGame,
   quitGame,
   gameLoop,
-  generateRandomNumber
-}
+  generateRandomNumber,
+};
