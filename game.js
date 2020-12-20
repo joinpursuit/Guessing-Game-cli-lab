30061fa614d73f370ca3a83380e5c1dbfaf9d545
joshuamendez <joshuafmendez@gmail.com>
2020-12-20 14:43:30 -0500

    completed game

--- game.js
+++ game.js
@@ -7,7 +7,18 @@ const rls = require('readline-sync')
  * @returns {undefined}
  */
 const startGame = () => {
-
+  // console.log("Thank you for playing the ultimate guessing game.");
+  // console.log("I, the computer, will start with picking a number between 1 and 10,000");
+  // console.log("If You don't guess my number in 15 guesses or less I win");
+  // console.log("If you do, you win");
+  // console.log("With each guess I will tell you if you are too high or too low");
+  if(rls.keyInYN('Want to play a game?')){
+    console.log('Let\'s start!');
+    gameLoop();
+  }else{
+    console.log('Have a nice life!');
+    quitGame();
+  }
 }
 
 /**
@@ -17,7 +28,8 @@ const startGame = () => {
  * @returns {undefined}
  */
 const quitGame = () => {
-
+  console.log('Goodbye!');
+  process.exit();
 }
 
 /**
@@ -28,9 +40,54 @@ const quitGame = () => {
  * @returns {undefined}
  */
 const gameLoop = () => {
-
+  console.log('I have a random number in mind');
+  console.log('It\'s between 1 and 1000');
+  console.log("You have 10 guesses total");
+  let top = 1000;
+  let bottom = 1;
+  // Generate a random number and store it in a variable
+  // let randomNum = 5;
+  let randomNum = generateRandomNumber();
+  let guessCount = 10;
+  // Take an input (guess) from the user using rls.questionInt()
+  while(guessCount > 0){
+    let guess = rls.questionInt();
+    if(guess === randomNum){
+      gameOver(true);
+    }else{
+      guessCount--;
+      if(guessCount === 1){
+        console.log(`You have ${guessCount} guess left!`);
+      }else {
+        console.log(`You have ${guessCount} guesses left!`);
+      }
+      if(guess > randomNum){
+        console.log('You\'re guess is too high.');
+        top = randomNum - 1;
+      }else{
+        console.log('You\'re guess is too low.');
+        bottom = randomNum + 1;
+      }
+    }
+  }
+  gameOver(false);
 }
 
+const gameOver = humanWins => {
+  if(humanWins){
+    console.log('Congrats! You got it right!');
+  }else{
+    console.log('You lose!');
+    quitGame();
+  }  
+  if(rls.keyInYN('Want to play again?')){
+    console.log('Let\'s start!');
+    gameLoop();
+  }else{
+    console.log('Have a nice life!');
+    quitGame();
+  }
+}
 
 /***
  * Generates a random number 
@@ -38,7 +95,9 @@ const gameLoop = () => {
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
-
+  let num = Math.floor(Math.random() * 1000) + 1;
+  console.log(num); // tested to make sure random
+  return num;
 }
 
 startGame()
