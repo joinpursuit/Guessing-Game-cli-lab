9468ceef639b7a1e7cc3ebce75745a1ea1e1a2d0
Sarah Weinstein <sbweins@gmail.com>
2020-12-19 21:27:04 -0500

    Building Command Line Apps Lab

--- game.js
+++ game.js
@@ -8,6 +8,22 @@ const rls = require('readline-sync')
  */
 const startGame = () => {
 
+// prompt user if they want to play
+// if yes
+//console log "Let's Start!"
+// call gameLoop()
+// else
+// call quitGame()
+
+  let answer = rls.keyInYN("Ready?")
+  if (answer){
+    console.log("Let's start!")
+    gameLoop()
+  }
+  else {
+    console.log("Have a nice life!")
+    quitGame()
+  }
 }
 
 /**
@@ -17,7 +33,8 @@ const startGame = () => {
  * @returns {undefined}
  */
 const quitGame = () => {
-
+  console.log("Goodbye!")
+  process.exit()
 }
 
 /**
@@ -28,6 +45,23 @@ const quitGame = () => {
  * @returns {undefined}
  */
 const gameLoop = () => {
+  console.log("I have a random number in mind \n")
+  console.log("It's between 1 and 1000 \n")
+  console.log("You have 10 guesses \n")
+let randNum = generateRandomNumber()
+let guess = rls.questionInt("Give us your guess!")
+if (guess = 20) {
+  console.log ("Congrats! You got it right!")
+}
+else if (guess < 20) {
+  console.log ("Your guess is too high")
+  else {
+    console.log("Your guess is too low")
+  }
+}
+
+
+
 
 }
 
@@ -37,7 +71,8 @@ const gameLoop = () => {
  *
  * @returns {number} - a number between 1 and 1000
  */
-const generateRandomNumber = () => {
+const generateRandomNumber = () => { 
+  let () =
 
 }
 
