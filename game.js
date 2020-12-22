49de85bac4f523088c3db42247bb20ff00bd5c54
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2020-12-21 20:35:58 -0500

    Game working

--- game.js
+++ game.js
@@ -1,68 +1,79 @@
-const rls = require('readline-sync')
+const rls = require("readline-sync");
 
 /**
  * Starts the game by prompting the user if they want to play
  * Calls either gameLoop() or quitGame()
- * 
+ *
  * @returns {undefined}
  */
 const startGame = () => {
-  console.log("welcome")
+  console.log("welcome");
   let question = rls.keyInYN("Would you like to play");
-  if(question){
+  if (question) {
     console.log("Let's start!");
     gameLoop();
-  }else{
+  } else {
     console.log("Have a nice life!");
     quitGame();
   }
-
-
-}
+};
 
 /**
  * Logs "Goodbye!"
  * Calls process.exit() to quit the game
- * 
+ *
  * @returns {undefined}
  */
 const quitGame = () => {
- console.log("Goodbye!");
- process.exit();
-
-}
+  console.log("Goodbye!");
+  process.exit();
+};
 
 /**
  * Controls the flow of the game.
  * Should prompt the user to play again once all
  * guesses have been made or correct answer guessed
- * 
+ *
  * @returns {undefined}
  */
 const gameLoop = () => {
   console.log("I have a random number in mind");
   console.log("It's between 1 and 1000");
   console.log("You have 10 guesses total");
-}
-
+  const answer = generateRandomNumber();
+  let guess = rls.question();
+  for (let i = 1; i <= 10; i++)
+    if (guess === answer) {
+      console.log("Congrats! You got it right!");
+      break;
+    } else if (guess > answer) {
+      console.log("Your guess is too high");
+      gameLoop();
+    } else if (guess < answer) {
+      console.log("Your guess is too low");
+      gameLoop();
+    }
+    else console.log("You lose!");
+  quitGame();
+};
 
 /***
- * Generates a random number 
+ * Generates a random number
  *
  * @returns {number} - a number between 1 and 1000
  */
 const generateRandomNumber = () => {
-  let bottom = 1
-  let top = 1000
-  randomNum = Math.floor((bottom + top) / 2)
-  return randomNum
-}
+  let bottom = 1;
+  let top = 1000;
+  randomNum = Math.floor((bottom + top) / 2);
+  return randomNum;
+};
 
-startGame()
+startGame();
 
 module.exports = {
   startGame,
   quitGame,
   gameLoop,
-  generateRandomNumber
-}
+  generateRandomNumber,
+};
